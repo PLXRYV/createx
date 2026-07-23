9240b07e9985607f431f7a667b5884559b30e706
plxryv <yaroslav_52rus@mail.ru>
2026-07-23 18:21:44 +0300

    update project, added redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from '/src/App'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+
+import App from '/src/App';
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <App/>
-    </StrictMode>
-)
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>,
+);
